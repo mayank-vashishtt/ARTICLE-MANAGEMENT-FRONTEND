76047478eba4a7a44f5f91280d85424aa77159cb
mayank-vashishtt <145871109+mayank-vashishtt@users.noreply.github.com>
2024-09-21 03:34:21 +0530

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:5002/api',
+  baseURL: 'https://article-management-backend.onrender.com/api/',
 });
 
 api.interceptors.request.use((config) => {
